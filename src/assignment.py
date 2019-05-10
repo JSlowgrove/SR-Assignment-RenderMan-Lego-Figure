cab09f84a5b1b96d867a6623bd06e6f22a9a5ee1
Jamie Slowgrove <thearctictails@gmail.com>
2019-05-10 10:04:58 +0100

    Added definitons for the shaders along with shader files. The shader code is currently still just examples from the lecture notes but it is more for setting up the files for the moment. I have also reorgasnised the code so that the scene has its own definition.

--- src/assignment.py
+++ src/assignment.py
@@ -40,128 +40,224 @@ def checkAndCompileShader(shader) :
 		except subprocess.CalledProcessError :
 			sys.exit('shader compilation failed')
 
+# the definiton of the shader to use on the legs
+def legsShader() :
+	ri.Pattern('legsShader','legsShader', 
+	{ 
+		'string direction' : ["vertical"],
+		'color C1' : [1,0,0], 
+		'color C2' : [0,0,1]
+	})
+	ri.Bxdf('PxrSurface', 'plastic',
+	{
+		'reference color diffuseColor' : ['legsShader:Cout'],
+		'int diffuseDoubleSided' : [1]
 
-ri = prman.Ri() # create an instance of the RenderMan interface
-ri.Option("rib", {"string asciistyle": "indented"})
+	})
 
-filename = "assignment.rib"
-ri.ArchiveRecord(ri.COMMENT, 'assignment.rib')
+# the definiton of the shader to use on the head
+def headShader() :
+	ri.Pattern('headShader','headShader', 
+	{ 
+		'string direction' : ["vertical"],
+		'color C1' : [0,1,0], 
+		'color C2' : [1,0,0]
+	})
+	ri.Bxdf('PxrSurface', 'plastic',
+	{
+		'reference color diffuseColor' : ['headShader:Cout'],
+		'int diffuseDoubleSided' : [1]
 
-# Initalise Renderman in Python
-ri.Begin("__render") #filename)
-# Specify the output
-ri.Display("assignment.exr", "it", "rgba")
-# Specify 1080p resolution 1:1 pixel Aspect ratio
-ri.Format(1920,1080,1)
-# set the projection to perspective
-ri.Projection(ri.PERSPECTIVE,{ri.FOV:50}) 
+	})
 
-# set the depth of field (fstop, focal length, focal distance)
-#ri.DepthOfField(3.2,0.01,0.75)
+# the definiton of the shader to use on the chest
+def chestShader() :
+	ri.Pattern('chestShader','chestShader', 
+	{ 
+		'string direction' : ["vertical"],
+		'color C1' : [1,1,1], 
+		'color C2' : [0,1,0]
+	})
+	ri.Bxdf('PxrSurface', 'plastic',
+	{
+		'reference color diffuseColor' : ['chestShader:Cout'],
+		'int diffuseDoubleSided' : [1]
 
-# update render type to apply shadows
-ri.Hider('raytrace', {'int incremental' :[1]})
-ri.Integrator('PxrPathTracer', 'integrator')
+	})
 
-# Start the world
-ri.WorldBegin()
+# the definiton of the shader to use on the legs
+def tableShader() :
+	ri.Pattern('tableShader','tableShader', 
+	{ 
+		'string direction' : ["vertical"],
+		'color C1' : [0,1,0], 
+		'color C2' : [0,0,1]
+	})
+	ri.Bxdf('PxrSurface', 'plastic',
+	{
+		'reference color diffuseColor' : ['tableShader:Cout'],
+		'int diffuseDoubleSided' : [1]
 
-#hdr light & background
-ri.TransformBegin() #LIGHT BEGIN-----------------------------------------------{
-ri.Rotate(80,1,0,0)
-ri.Rotate(180,0,1,0)
-ri.Rotate(25,0,0,1)
-ri.Light("PxrDomeLight","hdrLight", {"float exposure" : [0], "string lightColorMap" : ["../img/hdr_4k.tx"]})
-ri.TransformEnd() #LIGHT End---------------------------------------------------{
+	})
 
-ri.ArchiveRecord(ri.COMMENT, 'move everything back')
-ri.Translate(0,0,4)
+# the definition of the scene to generate
+def drawScene(ri) :
+	##############################LEGO FIGURE BEGIN##############################
+	ri.ArchiveRecord(ri.COMMENT, 'lego figure group')
+	ri.TransformBegin()
+	ri.Translate(0,0,-1)
+	ri.Rotate(25, 0, 1, 0)
 
-ri.ArchiveRecord(ri.COMMENT, 'lego figure group')
-ri.TransformBegin() #LEGO FIGURE BEGIN-----------------------------------------{
-ri.Translate(0,0,-1)
-ri.Rotate(25, 0, 1, 0)
+	#********************************HEAD BEGIN**********************************
+	ri.ArchiveRecord(ri.COMMENT, 'head')
+	# set the shader to use
+	headShader()
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'head'})
+	ri.TransformBegin() 
+	ri.Translate(-0.1,0.5,0.1)
+	ri.Rotate(90,1,0,0)
+	ri.Cylinder(0.15,-0.125,0.125,360)
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	#*********************************HEAD END***********************************
 
-ri.ArchiveRecord(ri.COMMENT, 'head')
-ri.TransformBegin() #HEAD BEGIN--------------------------------------{
-ri.Translate(-0.1,0.5,0.1)
-ri.Rotate(90,1,0,0)
-ri.Cylinder(0.15,-0.125,0.125,360)
-ri.TransformEnd() #HEAD END------------------------------------------}
+	#*******************************CHEST BEGIN**********************************
+	ri.ArchiveRecord(ri.COMMENT, 'chest')
+	# set the shader to use
+	chestShader()
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'chest'})
+	ri.TransformBegin() 
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	#********************************CHEST END**********************************
 
-ri.ArchiveRecord(ri.COMMENT, 'chest')
-ri.TransformBegin() #CHEST BEGIN-------------------------------------{
-ri.TransformEnd() #CHEST END-----------------------------------------}
+	#********************************LEGS BEGIN**********************************
+	ri.ArchiveRecord(ri.COMMENT, 'legs group')
+	# set the shader to use
+	legsShader()
+	ri.TransformBegin()
 
+	#-------------------------------WAIST BEGIN----------------------------------
+	ri.ArchiveRecord(ri.COMMENT, 'waist')
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'waistLeg'})
+	ri.TransformBegin() 
+	ri.Translate(-0.12,-0.5,0.1)
+	Cube(0.43,0.1,0.3)
+	ri.Translate(-0.003,-0.145,0)
+	ri.Rotate(90,0,1,0)
+	ri.Cylinder(0.14,-0.02,0.02,360)
+	ri.Translate(0,0,0.02)
+	ri.Disk(0,0.14,360)
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	#--------------------------------WAIST END-----------------------------------
 
-checkAndCompileShader('legsShader')
+	#------------------------------LEFT LEG BEGIN--------------------------------
+	ri.ArchiveRecord(ri.COMMENT, 'left leg')
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'leftLeg'})
+	ri.TransformBegin() 
+	ri.Translate(0,-1.0,0)
+	Cube(0.2,0.1,0.1)
+	ri.Translate(0,0.15,0.15)
+	Cube(0.2,0.4,0.2)
+	ri.Translate(0,0.2,-0.04)
+	ri.Rotate(90,0,1,0)
+	ri.Cylinder(0.14,-0.1,0.1,360)
+	ri.Translate(0,0,0.1)
+	ri.Disk(0,0.14,360)
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	#-------------------------------LEFT LEG END---------------------------------
 
-ri.Pattern('legsShader','legsShader', 
-{ 
-	'string direction' : ["vertical"],
-	'color C1' : [0,0,1], 
-	'color C2' : [0,1,0]
-})
-ri.Bxdf('PxrSurface', 'plastic',
-{
-	'reference color diffuseColor' : ['legsShader:Cout'],
-	'int diffuseDoubleSided' : [1]
+	#-----------------------------RIGHT LEG BEGIN--------------------------------
+	ri.ArchiveRecord(ri.COMMENT, 'right leg')
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'rightLeg'})
+	ri.TransformBegin()
+	ri.Translate(-0.25,-1.0,0)
+	Cube(0.2,0.1,0.1)
+	ri.Translate(0,0.15,0.15)
+	Cube(0.2,0.4,0.2)
+	ri.Translate(0,0.2,-0.04)
+	ri.Rotate(90,0,1,0)
+	ri.Cylinder(0.14,-0.1,0.1,360)
+	ri.Translate(0,0,0.1)
+	ri.Disk(0,0.14,360)
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	#------------------------------RIGHT LEG END---------------------------------
 
-})
+	ri.TransformEnd()
+	#*********************************LEGS END***********************************
 
+	ri.TransformEnd() 
+	###############################LEGO FIGURE END###############################
 
+	#################################TABLE BEGIN#################################
+	ri.ArchiveRecord(ri.COMMENT, 'table')
+	# set the shader to use
+	tableShader()
+	ri.AttributeBegin()
+	ri.Attribute( 'identifier',{ 'name' :'table'})
+	ri.TransformBegin() 
+	ri.Rotate(20,0,1,0)
+	ri.Translate(-3,-1.3,-1.5)
+	Cube(8,0.5,5)
+	ri.TransformEnd() 
+	ri.AttributeEnd()
+	###################################TABLE END###################################
 
+# the main function
+if __name__ == '__main__':
+	ri = prman.Ri() # create an instance of the RenderMan interface
+	ri.Option("rib", {"string asciistyle": "indented"})
 
-ri.ArchiveRecord(ri.COMMENT, 'legs group')
-ri.TransformBegin() #LEGS BEGIN--------------------------------------{
+	filename = "assignment.rib"
+	ri.ArchiveRecord(ri.COMMENT, 'assignment.rib')
 
-ri.ArchiveRecord(ri.COMMENT, 'waist')
-ri.TransformBegin() #WAIST BEGIN---------------------------{
-ri.Translate(-0.12,-0.5,0.1)
-Cube(0.43,0.1,0.3)
-ri.Translate(-0.003,-0.145,0)
-ri.Rotate(90,0,1,0)
-ri.Cylinder(0.14,-0.02,0.02,360)
-ri.Translate(0,0,0.02)
-ri.Disk(0,0.14,360)
-ri.TransformEnd() #WAIST END-------------------------------}
+	# Initalise Renderman in Python
+	ri.Begin("__render") #filename)
+	# Specify the output
+	ri.Display("assignment.exr", "it", "rgba")
+	# Specify 1080p resolution 1:1 pixel Aspect ratio
+	ri.Format(1920,1080,1)
+	# set the projection to perspective
+	ri.Projection(ri.PERSPECTIVE,{ri.FOV:50}) 
 
-ri.ArchiveRecord(ri.COMMENT, 'left leg')
-ri.TransformBegin() #LEFT LEG BEGIN------------------------{
-ri.Translate(0,-1.0,0)
-Cube(0.2,0.1,0.1)
-ri.Translate(0,0.15,0.15)
-Cube(0.2,0.4,0.2)
-ri.Translate(0,0.2,-0.04)
-ri.Rotate(90,0,1,0)
-ri.Cylinder(0.14,-0.1,0.1,360)
-ri.Translate(0,0,0.1)
-ri.Disk(0,0.14,360)
-ri.TransformEnd() #LEFT LEG END----------------------------}
+	# set the depth of field (fstop, focal length, focal distance)
+	#ri.DepthOfField(3.2,0.01,0.75)
 
-ri.ArchiveRecord(ri.COMMENT, 'right leg')
-ri.TransformBegin() #RIGHT LEG BEGIN-----------------------{
-ri.Translate(-0.25,-1.0,0)
-Cube(0.2,0.1,0.1)
-ri.Translate(0,0.15,0.15)
-Cube(0.2,0.4,0.2)
-ri.Translate(0,0.2,-0.04)
-ri.Rotate(90,0,1,0)
-ri.Cylinder(0.14,-0.1,0.1,360)
-ri.Translate(0,0,0.1)
-ri.Disk(0,0.14,360)
-ri.TransformEnd() #RIGHT LEG END----------------------------}
+	# update render type to apply shadows
+	ri.Hider('raytrace', {'int incremental' :[1]})
+	ri.Integrator('PxrPathTracer', 'integrator')
 
-ri.TransformEnd() #LEGS END------------------------------------------}
+	# compile the shaders
+	checkAndCompileShader('legsShader')
+	checkAndCompileShader('headShader')
+	checkAndCompileShader('chestShader')
+	checkAndCompileShader('tableShader')
 
-ri.TransformEnd() #LEGO FIGURE END---------------------------------------------}
+	# Start the world
+	ri.WorldBegin()
 
-ri.ArchiveRecord(ri.COMMENT, 'table')
-ri.TransformBegin() #TABLE BEGIN-----------------------------------------------{
-ri.Rotate(20,0,1,0)
-ri.Translate(-3,-1.3,-1.5)
-Cube(8,0.5,5)
-ri.TransformEnd() #TABLE END---------------------------------------------------}
+	###################################LIGHT BEGIN###################################
+	#hdr light & background
+	ri.TransformBegin()
+	ri.Rotate(80,1,0,0)
+	ri.Rotate(180,0,1,0)
+	ri.Rotate(25,0,0,1)
+	ri.Light("PxrDomeLight","hdrLight", {"float exposure" : [0], "string lightColorMap" : ["../img/hdr_4k.tx"]})
+	ri.TransformEnd()
+	####################################LIGHT END####################################
 
-ri.WorldEnd()
-ri.End()
+	ri.ArchiveRecord(ri.COMMENT, 'move everything back')
+	ri.Translate(0,0,4)
+
+	drawScene(ri)
+
+	ri.WorldEnd()
+	ri.End()
